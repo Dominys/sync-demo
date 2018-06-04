2f4628c4851f527ae280dc30ead924b6eea0b5e3
Dominys <roman.bezpalko@kenshoo.com>
2018-06-04 14:24:35 +0300

    Change to stream

--- src/main/java/com/sync/demo/syncdemo/FbStreamProcessor.java
+++ src/main/java/com/sync/demo/syncdemo/FbStreamProcessor.java
@@ -2,20 +2,40 @@ package com.sync.demo.syncdemo;
 
 import com.sync.demo.rest.demorest.DownloadRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.apache.kafka.streams.kstream.ValueTransformer;
+import org.apache.kafka.streams.kstream.ValueTransformerSupplier;
+import org.apache.kafka.streams.processor.ProcessorContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsProcessor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+/**
+ * @author rbezpalko
+ * @since 22.05.18
+ */
+
+@EnableBinding(KafkaStreamsProcessor.class)
 @Slf4j
-//@Component
-public class FbProcessor {
+public class FbStreamProcessor {
+
+    AtomicInteger counter = new AtomicInteger(0);
 
     public static final String ASSET_FEED_FIELDS = "asset_feed_id,asset_feed_spec"+
             "{" +
@@ -45,20 +65,21 @@ public class FbProcessor {
             ",name,tracking_specs,"
             + "updated_time,created_time,view_tags,ad_review_feedback,bid_amount";
 
-    @Autowired
-    private KafkaTemplate<String, DownloadResponse> kafkaTemplate;
+    @StreamListener("input")
+    @SendTo("output")
+    public KStream<?, DownloadResponse> process(KStream<Object, DownloadRequest> input) {
 
-    @KafkaListener(topics = "download-request", groupId = "download-request", containerFactory = "kafkaListenerContainerFactory")
-    public void process(DownloadRequest message) {
-        log.info("Start job {} with ids {}", message.getJobId(), message.getIds());
-        Map<String, Object> response = request(message);
-        log.info("Got response job {}", message.getJobId());
-        message.getIds().stream()
-                .map(id -> {
-                    Object data = response.get(id);
-                    return new DownloadResponse(message.getJobId(), id, data != null ? Status.SUCCESS : Status.MISSED, data);
-                })
-                .forEach(r -> kafkaTemplate.send("downloaded-ads", r));
+
+        return input
+                .flatMapValues(value -> {
+                    log.info("Start inc {} job {} with ids {} threadId {}", counter.incrementAndGet(), value.getJobId(), value.getIds(), Thread.currentThread().getId());
+                    Map<String, Object> response = request(value);
+                    return value.getIds().stream()
+                            .map(id -> {
+                                Object data = response.get(id);
+                                return new DownloadResponse(value.getJobId(), id, data != null ? Status.SUCCESS : Status.MISSED, data); })
+                            .collect(Collectors.toList());
+                });
     }
 
     private Map<String, Object> request(DownloadRequest request) {
@@ -73,16 +94,13 @@ public class FbProcessor {
                 .retrieve()
                 .bodyToFlux(new ParameterizedTypeReference<Map<String, Object>>() {})
                 .onErrorReturn(Collections.emptyMap())
-                .blockFirst();
+                .blockLast();
     }
 
-    private ExchangeFilterFunction logRequest() {
-        return (clientRequest, next) -> {
-            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
-//            clientRequest.headers()
-//                    .forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
-            return next.exchange(clientRequest);
-        };
+    private static ExchangeFilterFunction logRequest() {
+        //            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+//            clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+        return ExchangeFilterFunction.ofRequestProcessor(Mono::just);
     }
 
 
